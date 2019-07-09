6c644190f28861142e8a1c90144832ecc2b11dba
王永晶 <2018559495@qq.com>
2019-07-09 20:40:50 +0800

    [王永晶]更自己的Controller文件

--- src/main/java/com/briup/apps/sms/bean/User.java
+++ src/main/java/com/briup/apps/sms/bean/User.java
@@ -1,5 +1,39 @@
 package com.briup.apps.sms.bean;
 
-public class User {
-
+public class User{
+	private long id;
+	private String name;
+	private Integer age;
+	private String gender;
+	private String telephone;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
 }
