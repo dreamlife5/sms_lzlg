b123f0569396da0ccb116a3e5ce71a02c40bb1cd
Administrator <Administrator@GQSI1X6Z4YHY63B>
2019-07-10 09:20:23 +0800

    []wangjc

--- src/main/java/com/briup/apps/sms/bean/StudentCourse.java
+++ src/main/java/com/briup/apps/sms/bean/StudentCourse.java
@@ -1,5 +1,11 @@
 package com.briup.apps.sms.bean;
 
 public class StudentCourse {
-
+     private long id;
+     private long user_id;     
+     private long course_id;
+     private String xk_time;
+     private String grade;
 }
+
+
