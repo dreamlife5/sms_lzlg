69b4b39ff96ad23080f1647a89081fdd0a9c8553
王永晶 <2018559495@qq.com>
2019-07-10 10:19:02 +0800

    [王永晶]修改数据类型

--- src/main/java/com/briup/apps/sms/bean/User.java
+++ src/main/java/com/briup/apps/sms/bean/User.java
@@ -1,12 +1,18 @@
 package com.briup.apps.sms.bean;
 
 public class User{
-	private long id;
+	private Long id;
 	private String name;
 	private Integer age;
 	private String gender;
 	private String telephone;
-	private long clazz_id;
+	private Long clazz_id;
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public void setClazz_id(Long clazz_id) {
+		this.clazz_id = clazz_id;
+	}
 	public long getClazz_id() {
 		return clazz_id;
 	}
