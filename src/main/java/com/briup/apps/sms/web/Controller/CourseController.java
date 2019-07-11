1395241246088862efb19b2f7e6cf558d6e25d90
张成 <zhangcheng1996603@qq.com>
2019-07-11 09:57:32 +0800

    [张成]提交代码

--- src/main/java/com/briup/apps/sms/web/Controller/CourseController.java
+++ src/main/java/com/briup/apps/sms/web/Controller/CourseController.java
@@ -1,5 +1,24 @@
 package com.briup.apps.sms.web.Controller;
 
-public class CourseController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.briup.apps.sms.bean.Course;
+import com.briup.apps.sms.service.CourseService;
+
+@RestController
+@RequestMapping("/course")
+public class CourseController {
+	@Autowired
+	private CourseService courseService;
+	
+	// http://localhost:8080/school/selectAll
+	@GetMapping("selectAll")
+	public List<Course> selectAll(){
+		return courseService.selectAll();
+	}
 }
