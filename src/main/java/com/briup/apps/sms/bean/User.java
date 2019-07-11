e2753b939e60117d230b24619b4edc687b84ad01
王永晶 <2018559495@qq.com>
2019-07-11 08:56:06 +0800

    【王永晶】最终程序框架

--- src/main/java/com/briup/apps/sms/bean/User.java
+++ src/main/java/com/briup/apps/sms/bean/User.java
@@ -5,6 +5,13 @@ public class User{
 	private String realname;
 	private String password;
 	private String student_no;
+	private String gender;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	public String getRealname() {
 		return realname;
 	}
@@ -23,37 +30,18 @@ public class User{
 	public void setStudent_no(String student_no) {
 		this.student_no = student_no;
 	}
-	private String gender;
-	private String telephone;
-	private Long clazz_id;
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public void setClazz_id(Long clazz_id) {
-		this.clazz_id = clazz_id;
-	}
-	public long getClazz_id() {
-		return clazz_id;
-	}
-	public void setClazz_id(long clazz_id) {
-		this.clazz_id = clazz_id;
-	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
 	public String getGender() {
 		return gender;
 	}
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-	public String getTelephone() {
-		return telephone;
+	public Long getClazz_id() {
+		return clazz_id;
 	}
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
+	public void setClazz_id(Long clazz_id) {
+		this.clazz_id = clazz_id;
 	}
+	private Long clazz_id;
+	
 }
