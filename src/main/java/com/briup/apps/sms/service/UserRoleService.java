9a43441288ed4480944aac5cc9f6923494d1ff43
温盛隆 <2321131255@qq.com>
2019-07-11 11:04:19 +0800

    [温盛隆]提交代码

---
+++ src/main/java/com/briup/apps/sms/service/UserRoleService.java
@@ -0,0 +1,11 @@
+package com.briup.apps.sms.service;
+
+import java.util.List;
+
+import com.briup.apps.sms.bean.UserRole;
+
+public interface UserRoleService {
+    List<UserRole> selectAll();
+    void insert(UserRole UserRole) throws Exception;
+}
+
